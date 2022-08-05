a62aa0315d893848faf6d21d1b929491f373d376
ariel11 <arielgelberg@gmail.com>
2022-08-05 14:37:32 +0300

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,147 +1,148 @@
-use std::{collections::HashMap};
-use std::hash::Hash;
-use std::ops::Deref;
-use crate::Node::{Cons, NULL}; 
+use std::collections::HashMap;
+use std::boxed::Box;
+use std::hash::{Hash, Hasher};
+use std::ptr;
+use std::fmt::Debug;
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Node<T> 
-{
-    Cons(T, Option<Box<Node<T>>>),
-    NULL
-}
-impl<T> Deref for Node<T> 
-{
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Cons(x, _) => x,
-            NULL => panic!("NULL dereferenced")
-        }
-    }
+#[derive(Debug)]
+struct KeyRef<K> {
+  key: *const K,
 }
 
-impl<T: Clone> Node<T> 
-{   
-    pub fn new(x: T) -> Self {
-        Cons(x, None)
-    }
-    pub fn new_with_next(x: T, next: Box<Node<T>>) -> Self {
-        Cons(x, Some(next))
-    }
-    pub fn set_next_raw(&mut self, next: Box<Node<T>>) {
-        match self {
-            Cons(_, ref mut next_mut) => *next_mut = Some(next),
-            NULL => panic!("NULL dereferenced")
-        }
-    }
+impl <K: Hash> Hash for KeyRef<K> {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    unsafe { (*self.key).hash(state) }
+  }
+}
 
-    pub fn set_next(&mut self, next_data: T) 
-    {
-        match self {
-            Cons(_, next) => {
-                *next = Some(Box::new(Cons(next_data, None)));
-            }
-            NULL => panic!("NULL dereferenced")
-        }
-    }
+impl <K: PartialEq> PartialEq for KeyRef<K> {
+  fn eq(&self, other: &Self) -> bool {
+    unsafe { (*self.key).eq(&*other.key) }
+  }
+}
 
-    pub fn get_next(&self) -> &Option<Box<Node<T>>> 
-    {
-        match self {
-            Cons(_, next) => {
-                next
-            }
-            _ => &None
-        }
+impl <K: Eq> Eq for KeyRef<K> {}
+
+#[derive(Debug)]
+struct CacheEntry<K, V> {
+  key: K,
+  val: V,
+  prev: *mut CacheEntry<K, V>,
+  next: *mut CacheEntry<K, V>,  
+}
+
+impl <K: Default, V: Default> Default for CacheEntry<K, V> {
+  fn default() -> Self {
+    CacheEntry {
+      key: Default::default(),
+      val: Default::default(),
+      prev: ptr::null_mut(),
+      next: ptr::null_mut(),
     }
-    pub fn set_value(&mut self, value: T) 
-    {
-        match self {
-            Cons(_, _) => {
-                *self = Cons(value, None);
-            }
-            NULL => panic!("NULL dereferenced")
-        }
+  }
+}
+
+struct LRUCacheBase<K, V> {
+  map: HashMap<KeyRef<K>, Box<CacheEntry<K, V>>>,
+  head: *mut CacheEntry<K, V>,
+  capacity: usize,
+}
+
+impl<K, V> LRUCacheBase<K, V> where K: Default+Hash+Eq+Debug, V: Default+Hash+Debug  {
+  pub fn new(capacity: usize) -> Self {
+    let mut cache = LRUCacheBase {
+      map: HashMap::new(),
+      head: Box::into_raw(Box::new(Default::default())),
+      capacity: capacity,
+    };
+    unsafe {
+      (*cache.head).next = cache.head;
+      (*cache.head).prev = cache.head;
     }
-    pub fn get_value(&self) -> &T 
-    {
-        match self {
-            Cons(x, _) => x,
-            NULL => panic!("NULL dereferenced")
-        }
+    return cache;
+  }
+  
+  pub fn get<'a>(&'a mut self, key: &K) -> Option<&'a V> {
+    match self.map.get_mut(&KeyRef{key: key}) {
+      Some(node) => {
+        let entry: *mut CacheEntry<K, V> = &mut **node;
+        self.remove_node(entry);
+        self.add_node(entry);
+        return unsafe { Some(&(*entry).val) }
+      },
+      None => return None,
     }
-    pub fn not_null(&self) -> bool {
-        match self {
-            NULL => false,
-            _ => true
+  }
+  
+  pub fn put(&mut self, key: K, value: V) {
+    match self.map.get_mut(&KeyRef{key: &key}) {
+      Some(node) => {
+        let entry: *mut CacheEntry<K, V> = &mut **node;
+        self.remove_node(entry);
+        self.add_node(entry);
+        unsafe { (*entry).val = value; }
+      },
+      None => {
+        if self.map.len() == self.capacity {
+          unsafe {
+            let entry: *mut CacheEntry<K, V> = (*self.head).prev;
+            self.remove_node(entry);
+            self.map.remove(&KeyRef{key: &(*entry).key});
+          }
         }
+        let mut entry: Box<CacheEntry<K, V>> = Box::new(Default::default());
+        (*entry).val = value;
+        (*entry).key = key;
+        self.add_node(&mut *entry);
+        self.map.insert(KeyRef{key: &(*entry).key}, entry);
+      },
+    }
+  }
+
+  fn add_node(&mut self, node: *mut CacheEntry<K, V>) {
+    unsafe {
+      (*node).next = (*self.head).next;
+      (*node).prev = self.head;
+      (*(*self.head).next).prev = node;
+      (*self.head).next = node;
+    }
+  }
+
+  fn remove_node(&mut self, node: *mut CacheEntry<K, V>) {
+    unsafe {
+      (*(*node).next).prev = (*node).prev;
+      (*(*node).prev).next = (*node).next;
+    }
+  }
+
+}
+
+impl<K, V> Drop for LRUCacheBase<K, V> {
+  fn drop(&mut self) {
+    unsafe {
+        Box::from_raw(self.head);
+    }
+  }
+}
+
+struct LRUCache {
+  cache: LRUCacheBase<i32, i32>,
+}
+
+
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+      LRUCache {
+        cache: LRUCacheBase::new(capacity as usize),
+      }
     }
     
-
-}
-
-
-pub struct LRUCache<T: Clone + Hash + Eq> 
-{
-    len: usize,
-    capacity: usize,
-    cache: HashMap<T, Box<Node<T>>>,
-    head: Box<Node<T>>,
-    tail: Box<Node<T>>,
-}
-impl<T: Clone + Hash + Eq> LRUCache<T> 
-{
-    pub fn new(capacity: usize) -> Self {
-        LRUCache {
-            len: 0,
-            capacity,
-            cache: HashMap::new(),
-            head: Box::new(NULL),
-            tail: Box::new(NULL)
-        }
-    }
-    pub fn put(&mut self, key: T, value: T) {
-        self.cache.insert(key, Box::new(Node::new(value)));
-    }
-
-    pub fn get(&mut self, key: &T) -> &T {
-        return self.cache.get(key).unwrap().get_value();
-    }
-    pub fn get_mut(&mut self, key: &T) -> Option<&mut Box<Node<T>>> {
-        return self.cache.get_mut(key);
-    }
-    pub fn insert(&mut self, key: &T) -> Option<&mut Box<Node<T>>> {
-        let new_node = Box::new(Node::new(key.clone()));
-        self.cache.insert(key.clone(), new_node);
-        self.len += 1;
-        return self.cache.get_mut(key);
-    }
-    pub fn remove(&mut self, key: &T) -> Option<Box<Node<T>>> {
-        let node = self.cache.remove(key);
-        if node.clone().is_some() {
-            self.len -= 1;
-            self.remove_from_head(node.clone().unwrap());
-        }
-        return node;
-    }
-    pub fn remove_from_head(&mut self, node: Box<Node<T>>) {
-        if self.head.get_next().is_some() {
-            self.head.set_next_raw(node.clone());
-        } else {
-            self.tail = node;
-        }
-    }
-    pub fn add_to_head(&mut self, data: &T) {
-        if self.head.get_next().is_none() {
-            self.tail = Box::new(Node::new(data.clone()));
-        }
-        
-
+    fn get(&mut self, key: i32) -> i32 {
+        self.cache.get(&key).copied().unwrap_or(-1)
     }
     
-
-    
+    fn put(&mut self, key: i32, value: i32) {
+        self.cache.put(key, value);
+    }
 }
-
-
-
